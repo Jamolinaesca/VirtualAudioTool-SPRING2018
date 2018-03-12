f8d1dd1fdbd9452ca2aacbcb3973d55ae647a1f0
AD\jumo6105exc <jamolinaescalante@gmail.com>
2018-03-12 11:54:37 -0600

    Initial

---
+++ Assets/RHC/defaultState.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class defaultState : State
+{
+    public static defaultState instance;
+
+    Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;      //getting a call directly from the trigger and mapping to a variable
+    Valve.VR.EVRButtonId touchpadButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;    //getting a call directly from the touchpad and mapping to a variable
+
+    public defaultState(RightHand controller) : base(controller) {
+    }
+
+    public override void OnStateEnter()
+    {
+        Debug.Log("You are on the default state right now!");
+    }
+
+    public override void OnStateExit()
+    {
+        Debug.Log("You've exited the default state!");
+    }
+
+    public override void Tick(SteamVR_TrackedObject trackedObj, SteamVR_Controller.Device rightHand, GameObject hand, GameObject mic)
+    {
+        rightHand = SteamVR_Controller.Input((int)trackedObj.index); //get integer representation of the controller
+
+        if (rightHand.GetPressDown(triggerButton))
+        {
+            Debug.Log("Select!");
+        }
+        else if (rightHand.GetPressDown(touchpadButton))
+        {
+            Debug.Log("You have placed the note");
+
+
+        }
+    }
+
+    public void ChangeState()
+    {
+        Debug.Log("Hey!");
+        controller.SetState(new recordingState(controller));
+    }
+}
---
+++ Assets/raycastController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycastController : MonoBehaviour
+{
+	void Update ()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward,Color.green);
+	}
+}
